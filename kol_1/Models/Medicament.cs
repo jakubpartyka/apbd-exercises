74666b256eed8540678fe98a0d5203e3fe4caac7
Jakub Partyka <j.partyka20@gmail.com>
2020-04-23 13:15:39 +0200

    Update Medicament.cs

--- kol_1/Models/Medicament.cs
+++ kol_1/Models/Medicament.cs
@@ -5,25 +5,11 @@ using System.Threading.Tasks;
 namespace Kolokwium1.Models
 
 {
-
     public class Medicament
-
     {
-
-
-
         public int IdMedicament { get; set; }
-
-
-
         public string Name { get; set; }
-
-
-
         public string Description { get; set; }
-
         public string Type { get; set; }
-
     }
-
 }
