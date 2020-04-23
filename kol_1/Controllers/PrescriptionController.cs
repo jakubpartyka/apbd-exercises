8315f0eefa62b528119b0fc9cff603da883a0b9b
Jakub Partyka <j.partyka20@gmail.com>
2020-04-23 13:00:11 +0200

    Zapytanie o recepte dodano

--- kol_1/Controllers/PrescriptionController.cs
+++ kol_1/Controllers/PrescriptionController.cs
@@ -29,16 +29,52 @@ namespace Kolokwium1.Controllers
                     
                     com.Parameters.AddWithValue("IdPrescription", id);
 
-                    com.CommandText = "select Name as 'lek',Type as 'rodzaj',Date as 'data wystawienia',DueDate as 'data waznosci',
-Description as opis,Dose as dawka, IdPatient as 'pacjent nr.',IdDoctor as 'Doktor nr.' from Prescription_Medicament
+                    com.CommandText = "select Name Type,Date ,DueDate ,
+Description ,Dose , IdPatient,IdDoctor from Prescription_Medicament
 JOIN Prescription on Prescription.IdPrescription = Prescription_Medicament.IdPrescription
 JOIN Medicament on Medicament.IdMedicament = Prescription_Medicament.IdMedicament where IdPrescription =@id";
 
                     con.Open();
 
                     SqlDataReader dr = com.ExecuteReader();
-        
-        
+                 
+                    
+                    while (dr.Read()){
+                        var recepta = new Prescriptions();
+                        recepta.IdPerscription = (int)dr["IdPrescription"];
+
+                        recepta.Date = DateTime.Parse(dr["Date"].ToString());
+
+                        recepta.DueTime = DateTime.Parse(dr["DueDate"].ToString());
+
+                        recepta.IdPatient = (int)dr["IdPatient"];
+
+                        recepta.IdDoctor = (int)dr["IdDoctor"];
+                        
+                        var permed = new PrescriptionsMedicament();
+
+                        var listaMed = new List<Medicament>();
+
+                        permed.IdPerscription = (int)dr["IdPrescription"]; ;
+
+                        permed.IdMedicament = (int)dr["IdMedicament"];
+
+                        listaMed.Dose = (int)dr["Dose"];
+
+                        listaMed.Details = (dr["Details"].ToString());
+                        
+                        lista.Add(permed);
+                     
+                        list.Add(recepta);   
+                    }
+
+                    return Ok(list + "\n" + lista);
+
+                }
+                catch (SqlException sql)
+                {
+                    return BadRequest("Wysłano nieprawidłowe zapytanie");
+                }
         }
         
     }
