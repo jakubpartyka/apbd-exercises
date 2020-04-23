f2e59d1239112c81e8eecd6717077fa86046a4f9
Jakub Partyka <j.partyka20@gmail.com>
2020-04-23 12:52:04 +0200

    Update PrescriptionController.cs

--- kol_1/Controllers/PrescriptionController.cs
+++ kol_1/Controllers/PrescriptionController.cs
@@ -1,1 +1,22 @@
+namespace Kolokwium1.Controllers
 
+{
+
+    [Route("api/prescriptions")]
+
+    [ApiController]
+
+    public class PrescriptionsController : ControllerBase
+
+    {
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetPerscription(int id)
+
+        {
+        }
+        
+    }
+    
+}
