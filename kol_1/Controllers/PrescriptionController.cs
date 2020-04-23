6be2e1c43bae77177de3ace42693fff6063a34c1
Jakub Partyka <j.partyka20@gmail.com>
2020-04-23 12:54:29 +0200

    query added

--- kol_1/Controllers/PrescriptionController.cs
+++ kol_1/Controllers/PrescriptionController.cs
@@ -13,8 +13,32 @@ namespace Kolokwium1.Controllers
         [HttpGet("{id}")]
 
         public IActionResult GetPerscription(int id)
-
         {
+        using (SqlConnection con = new SqlConnection("Data Source = db-mssql; Initial Catalog=s18830; Integrated Security=True"))
+                com.Connection = con;
+                using (SqlCommand com = new SqlCommand())
+
+                {
+
+                try
+
+                {
+                    var list = new List<Prescriptions>();
+
+                    var lista = new List<PrescriptionsMedicament>();
+                    
+                    com.Parameters.AddWithValue("IdPrescription", id);
+
+                    com.CommandText = "select Name as 'lek',Type as 'rodzaj',Date as 'data wystawienia',DueDate as 'data waznosci',
+Description as opis,Dose as dawka, IdPatient as 'pacjent nr.',IdDoctor as 'Doktor nr.' from Prescription_Medicament
+JOIN Prescription on Prescription.IdPrescription = Prescription_Medicament.IdPrescription
+JOIN Medicament on Medicament.IdMedicament = Prescription_Medicament.IdMedicament where IdPrescription =@id";
+
+                    con.Open();
+
+                    SqlDataReader dr = com.ExecuteReader();
+        
+        
         }
         
     }
