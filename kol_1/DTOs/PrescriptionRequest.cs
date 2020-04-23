d279e1462f52fb6180728db774a87ac3aaa25a43
Jakub Partyka <j.partyka20@gmail.com>
2020-04-23 13:09:11 +0200

    Update PrescriptionRequest.cs

--- kol_1/DTOs/PrescriptionRequest.cs
+++ kol_1/DTOs/PrescriptionRequest.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-namespace kol1.DTOs
+namespace Kolokwium1.DTOs
 {
     public class PrescriptionResponse
     {
